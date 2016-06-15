b95f9294c5a5fa4eba9e0b9fc8879e65b784e270
anantjoshi97 <anantjoshi97.iitb@gmail.com>
2016-06-16 02:28:04 +0530

    Add files via upload
    work in progress

--- pid_pdi_low_res.m
+++ pid_pdi_low_res.m
@@ -7,7 +7,7 @@ writePosition(sx,24.0/180.0);
 writePosition(sy,102.0/180.0);
 fprintf('arduino and servo\n');
 %%
-iter=500;
+iter=1000;
 
 ct_x=zeros([1 2]);
 ct_y=zeros([1 2]);
@@ -39,7 +39,9 @@ preview(cam)
 ix = 0; iy = 0;
 dx=0;
 dy=0;
-for i = 1.0:1.0:iter
+i=1.0;
+while(1)
+%for i = 1.0:1.0:iter
 %preview(cam);
 
 img = snapshot(cam);
@@ -51,6 +53,10 @@ pics{i}=img;
   'ObjectPolarity','dark','Sensitivity',0.80,'EdgeThreshold',0.3);
 %screening function
  
+if( (abs((centre_final(i,1) - ct(1))^2 + (centre_final(i,2) - ct(2))^2))^0.5  < 40)
+    break;
+end
+
 centre_final(i,1)=centers(1,1);
 centre_final(i,2)=centers(1,2);
 %elapsed = etime(clock, iTime); 
@@ -75,8 +81,8 @@ kpy= 0.00000119*147.8311*2;
 %kdx = 0; kdy = 0;
 %kix= -0.00000005;
 %kiy=  0.00000010;
-%kix= -0.0000009*246.0973*2/50;
-%kiy= 0.00000119*147.8311*2/50;
+kix= -0.0000009*246.0973*2/50;
+kiy= 0.00000119*147.8311*2/50;
 if(i~=1)
     if(px(i)*px(i-1)<0 && i~=1)
         ix = 0;
@@ -96,10 +102,11 @@ posy(i) = 104/180 + kpy*py(i) + kdy*dy  + kiy*iy ;
 writePosition(sx,posx(i));
 writePosition(sy,posy(i));
 
-
+i = i+1;
 %posy
 
 end
+
 %%
 writePosition(sx,24.0/180.0);
 writePosition(sy,104.0/180.0);
